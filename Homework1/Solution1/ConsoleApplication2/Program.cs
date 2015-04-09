ab3db5c3b5c4664e74c8441f4cf32a7ad49d6e19
Nickolay Okorokov <aargh11@bigmir.net>
2015-04-09 16:23:10 +0300

    Added files for solution homework 1

---
+++ Homework1/Solution1/ConsoleApplication2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string my_string1, my_string2, my_string3;
+            my_string1 = "\nмоя строка 1";
+            my_string2 = "\tмоя строка 2";
+            my_string3 = "\aмоя строка 3";
+            Console.WriteLine(my_string1);
+            Console.WriteLine(my_string2);
+            Console.WriteLine(my_string3);
+            Console.ReadLine();
+        }
+    }
+}
