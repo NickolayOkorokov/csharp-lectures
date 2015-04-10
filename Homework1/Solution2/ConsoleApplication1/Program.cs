e62a54610d205e851b44e702e25289e2012fcd5b
Nickolay Okorokov <aargh11@bigmir.net>
2015-04-10 12:06:26 +0300

    added second part of homework Conditions

---
+++ Homework1/Solution2/ConsoleApplication1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int operand1, operand2, equals;
+            string mathsign;
+            operand1 = 14;
+            operand2 = 7;
+
+            Console.Write("Enter math sign you want\n");
+            mathsign = Console.ReadLine();
+            switch (mathsign) { 
+                case "+":
+                    equals = operand1 + operand2;
+                    Console.WriteLine("{0} {1} {2} = {3}", operand1, mathsign, operand2, equals);
+                    break;
+                case "-":
+                    equals = operand1 - operand2;
+                    Console.WriteLine("{0} {1} {2} = {3}", operand1, mathsign, operand2, equals);
+                    break;
+                case "/":
+                    equals = operand1 / operand2;
+                    Console.WriteLine("{0} {1} {2} = {3}", operand1, mathsign, operand2, equals);
+                    break;
+                case "*":
+                    equals = operand1 * operand2;
+                    Console.WriteLine("{0} {1} {2} = {3}", operand1, mathsign, operand2, equals);
+                    break;
+                default:
+                    Console.WriteLine("Enter valid math sign");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework1/Solution2/ConsoleApplication2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float number;
+            Console.Write("Enter your number\n");
+            number = Int32.Parse(Console.ReadLine());
+            if (0 <= number & 14 >= number)
+                Console.WriteLine("You got to [0-14] gap");
+            else if (15 <= number & 35 >= number)
+                Console.WriteLine("You got to [15-35] gap");
+            else if (36 <= number & 50 >= number)
+                Console.WriteLine("You got to [36-50] gap");
+            else if (50 <= number & 100 >= number)
+                Console.WriteLine("You got to [50-100] gap");
+            else
+                Console.WriteLine("Please, enter another number");
+            Console.ReadKey();
+            }
+
+
+
+        }
+    }
+
